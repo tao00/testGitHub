87989be453024641864b1de64e56e615c0cee65c
tao <1009315169@qq.com>
2018-11-14 17:47:29 +0800

    golangStudy
    golangStudy

---
+++ m_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+import "fmt"
+
+func TestAbc(t *testing.T) {
+
+	fmt.Println("fmt output")
+	fmt.Print("abc")
+
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+type ABC interface {
+}
+
+func main() {
+
+	fmt.Println("Hello, World!")
+}
